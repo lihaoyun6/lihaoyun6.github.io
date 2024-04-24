1c27f26061c0a42fff88d7be648c7459ef4a38da
lihaoyun6 <lihaoyun11@gmail.comn>
2024-04-24 15:07:23 +0800

    update pages

--- quickrecorder/_next/static/lERdJDGMnHI0JdzJ2g9Ux/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/537-4238772a2c0a749c.js","static/chunks/pages/index-132cc7bf55d41fb0.js"],"/_error":["static/chunks/pages/_error-b9e1de59d6b43160.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
