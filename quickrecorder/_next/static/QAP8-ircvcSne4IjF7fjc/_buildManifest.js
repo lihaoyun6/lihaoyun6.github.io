d6d81811c438a6f98e60ccdcb8baff6e21698ccf
lihaoyun6 <lihaoyun11@gmail.comn>
2024-04-28 16:38:46 +0800

    update web page

---
+++ quickrecorder/_next/static/QAP8-ircvcSne4IjF7fjc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/537-4238772a2c0a749c.js","static/chunks/pages/index-c3aaf601dd90ad41.js"],"/_error":["static/chunks/pages/_error-b9e1de59d6b43160.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
